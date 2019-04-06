7f55f753a5b03524aab94f949d1f7c177717e248
YeemBoi <47729640+YeemBoi@users.noreply.github.com>
2019-04-05 21:31:26 -0600

    Update setupWordFinder.py

--- setupWordFinder.py
+++ setupWordFinder.py
@@ -2,7 +2,9 @@ print("Please paste the file path of your dictionary file. This file contains a 
 dictPath = input()
 
 print("Please paste the file path of the pyWordFinder folder.")
-pyPath = input()
+path = input()
+pyPath = path + "/wordFind.py"
+execPath = path[0:path.rfind("/")] + "/wordFind.bat"
 startPy = open(pyPath, "rt")
 startText = startPy.read()
 allText = 'dictionary = open("'+dictPath+'", "rt")\n' + startText
@@ -11,8 +13,7 @@ startPy = open(pyPath, "wt")
 startPy.write(allText)
 startPy.close()
 
-execPath = pyPath[0:pyPath.rfind("/")] + "/wordFind.bat"
 exec = open(execPath, "wt")
-exec.write("python3 " + pyPath + "/wordFind.py")
+exec.write("python3 " + pyPath)
 exec.close()
 print("Done! Your  program should be ready to use at", execPath)
