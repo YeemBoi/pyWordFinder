2d4ee6d90048a04053a60dba9c609d2a6c9baffb
YeemBoi <47729640+YeemBoi@users.noreply.github.com>
2019-04-07 10:01:00 -0600

    Update setupWordFinder.py

--- setupWordFinder.py
+++ setupWordFinder.py
@@ -1,19 +1,21 @@
+import os
+os.chdir(os.path.dirname(__file__))
+path = os.getcwd()
+
+pyPath = path+"/wordFind.py"
+execPath = path[0:path.rfind("/")]+"/wordFind.bat"
+execBat = open(execPath, "wt")
+execBat.write("python3 " + pyPath)
+execBat.close()
+
+startPy = open(path+"/src", "rt")
+startText = startPy.read()
+mainPy = open(pyPath, "wt")
+
 print("Please paste the file path of your dictionary file. This file contains a list of words.")
 dictPath = input()
 
-print("Please paste the file path of the pyWordFinder folder.")
-path = input()
-pyPath = path + "/wordFind.py"
-execPath = path[0:path.rfind("/")] + "/wordFind.bat"
-startPy = open(pyPath, "rt")
-startText = startPy.read()
-allText = 'dictionary = open("'+dictPath+'", "rt")\n' + startText
-
-startPy = open(pyPath, "wt")
-startPy.write(allText)
-startPy.close()
-
-exec = open(execPath, "wt")
-exec.write("python3 " + pyPath)
-exec.close()
+allText = 'dictionary = open("' +dictPath+ '", "rt")\n' + startText
+mainPy.write(allText)
+mainPy.close()
 print("Done! Your  program should be ready to use at", execPath)
